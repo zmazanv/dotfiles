223c695a1d3eec9b9a391725a9c551f3c139305e
Sebastián Zavala Villagómez <zmazanv@proton.me>
2023-08-27 06:30:16 -0400

    Refactoriza la configuración de Zsh

--- .zsh.d/rc.d/keybinds/keybinds.zsh
+++ .zsh.d/rc.d/keybinds/keybinds.zsh
@@ -1,59 +1,46 @@
-###############################################################################
+###############################################################
 # $HOME/.zsh.d/rc.d/keybinds/keybinds.zsh
 #
-# MAPAS DE TECLA
-###############################################################################
-# $HOME/.zsh.d/rc.d/keybinds/keybinds.zsh
+# zmazanv (Sebastián Zavala Villagómez)
+# [https://github.com/zmazanv]
+# [https://gitlab.com/zmazanv]
 #
-# KEYBINDS
-###############################################################################
-#------------------------------------------------------------------------------
-# Asigna la función `history-substring-search-up` para el modo normal.
-#------------------------------------------------------------------------------
-# Bind `history-substring-search-up` function in normal-mode.
-#------------------------------------------------------------------------------
-bindkey -M vicmd 'k' history-substring-search-up
-#------------------------------------------------------------------------------
-#
-#------------------------------------------------------------------------------
-# Asigna la función `history-substring-search-down` para el modo normal.
-#------------------------------------------------------------------------------
-# Bind `history-substring-search-down` function in normal-mode.
-#------------------------------------------------------------------------------
-bindkey -M vicmd 'j' history-substring-search-down
-#------------------------------------------------------------------------------
-#
-#------------------------------------------------------------------------------
-# Asigna la función `history-substring-search-up` para el modo de inserción.
-#------------------------------------------------------------------------------
-# Bind `history-substring-search-up` function in insert-mode.
-#------------------------------------------------------------------------------
-bindkey -M viins '^P' history-substring-search-up
-bindkey -M vicmd '^P' undefined-key
-#------------------------------------------------------------------------------
-#
-#------------------------------------------------------------------------------
-# Asigna la función `history-substring-search-down` para el modo de inserción.
-#------------------------------------------------------------------------------
-# Bind `history-substring-search-down` function in insert-mode.
-#------------------------------------------------------------------------------
-bindkey -M viins '^N' history-substring-search-down
-bindkey -M vicmd '^N' undefined-key
-#------------------------------------------------------------------------------
-#
-#------------------------------------------------------------------------------
-# Asigna la función `edit-command-line` para el modo normal.
-#------------------------------------------------------------------------------
+# ███████╗░██████╗██╗░░██╗
+# ╚════██║██╔════╝██║░░██║
+# ░░███╔═╝╚█████╗░███████║
+# ██╔══╝░░░╚═══██╗██╔══██║
+# ███████╗██████╔╝██║░░██║
+# ╚══════╝╚═════╝░╚═╝░░╚═╝
+# 
+# ██╗░░██╗███████╗██╗░░░██╗██████╗░██╗███╗░░██╗██████╗░░██████╗
+# ██║░██╔╝██╔════╝╚██╗░██╔╝██╔══██╗██║████╗░██║██╔══██╗██╔════╝
+# █████═╝░█████╗░░░╚████╔╝░██████╦╝██║██╔██╗██║██║░░██║╚█████╗░
+# ██╔═██╗░██╔══╝░░░░╚██╔╝░░██╔══██╗██║██║╚████║██║░░██║░╚═══██╗
+# ██║░╚██╗███████╗░░░██║░░░██████╦╝██║██║░╚███║██████╔╝██████╔╝
+# ╚═╝░░╚═╝╚══════╝░░░╚═╝░░░╚═════╝░╚═╝╚═╝░░╚══╝╚═════╝░╚═════╝░
+###############################################################
+
+# █▀▀ █▀▄ █ ▀█▀
+# ██▄ █▄▀ █ ░█░
 # Bind `edit-command-line` function in normal-mode.
-#------------------------------------------------------------------------------
 bindkey -M vicmd 'gi' edit-command-line
-#------------------------------------------------------------------------------
-#
-#------------------------------------------------------------------------------
-# Asigna la función `beginning-of-line` para el modo normal.
-#------------------------------------------------------------------------------
+
+# █░█ █ █▀ ▀█▀ █▀█ █▀█ █▄█   █▀ █░█ █▄▄ █▀ ▀█▀ █▀█ █ █▄░█ █▀▀
+# █▀█ █ ▄█ ░█░ █▄█ █▀▄ ░█░   ▄█ █▄█ █▄█ ▄█ ░█░ █▀▄ █ █░▀█ █▄█
+# Bind `history-substring-search-up` function in normal-mode to `K`.
+bindkey -M vicmd 'k' history-substring-search-up
+# Bind `history-substring-search-down` function in normal-mode to `J`.
+bindkey -M vicmd 'j' history-substring-search-down
+# Nullify `history-substring-search-up` function in normal-mode.
+bindkey -M vicmd '^P' undefined-key
+# Nullify `history-substring-search-down` function in normal-mode.
+bindkey -M vicmd '^N' undefined-key
+# Bind `history-substring-search-up` function in insert-mode to `CTRL+P`.
+bindkey -M viins '^P' history-substring-search-up
+# Bind `history-substring-search-down` function in insert-mode to `CTRL+N`.
+bindkey -M viins '^N' history-substring-search-down
+
+# █▀▄▀█ █▀█ █░█ █▀▀ █▀▄▀█ █▀▀ █▄░█ ▀█▀
+# █░▀░█ █▄█ ▀▄▀ ██▄ █░▀░█ ██▄ █░▀█ ░█░
 # Bind `beginning-of-line` function in normal-mode.
-#------------------------------------------------------------------------------
 bindkey -M vicmd '_' beginning-of-line
-#------------------------------------------------------------------------------
-###############################################################################
