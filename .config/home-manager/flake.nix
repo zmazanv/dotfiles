052f97314a72c2cdc3eb9c7804648b1a2415d00c
Sebastián Zavala Villagómez <zmazanv@proton.me>
2023-12-31 06:43:34 -0500

    Reformatea `home-manager/flake.nix`

--- .config/home-manager/flake.nix
+++ .config/home-manager/flake.nix
@@ -15,15 +15,17 @@
       system = "x86_64-linux";
       pkgs = nixpkgs.legacyPackages.${system};
     in {
-      homeConfigurations."sebas" = home-manager.lib.homeManagerConfiguration {
-        inherit pkgs;
+      homeConfigurations = {
+        sebas = home-manager.lib.homeManagerConfiguration {
+            inherit pkgs;
 
-        # Specify your home configuration modules here, for example,
-        # the path to your home.nix.
-        modules = [ ./home.nix ];
+            # Specify your home configuration modules here, for example,
+            # the path to your home.nix.
+            modules = [ ./home.nix ];
 
-        # Optionally use extraSpecialArgs
-        # to pass through arguments to home.nix
+            # Optionally use extraSpecialArgs
+            # to pass through arguments to home.nix
+        };
       };
     };
 }
