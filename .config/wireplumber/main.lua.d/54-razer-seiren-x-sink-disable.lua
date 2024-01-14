119a128d7094c8d2f86137a94dea17b1761a7a13
Sebastián Zavala Villagómez <zmazanv@proton.me>
2024-01-14 17:55:17 -0500

    Reformatea la configuración de Wireplumber con Stylua

--- .config/wireplumber/main.lua.d/54-razer-seiren-x-sink-disable.lua
+++ .config/wireplumber/main.lua.d/54-razer-seiren-x-sink-disable.lua
@@ -3,24 +3,24 @@ rule = {
     {
       -- Se identificará el nódulo de la salida de audio del micrófono Razer Seiren X emparejando su nombre de nódulo.
       -- The Razer Seiren X microphone audio sink node will be identified by matching its node name.
-      { "node.name", "matches", "alsa_output*Razer*Seiren*X*" },
+      { 'node.name', 'matches', 'alsa_output*Razer*Seiren*X*' },
     },
     -- O
     -- OR
     {
       -- Se identificará el nódulo de la entrada de audio del micrófono Razer Seiren X emparejando exactamente su apodo de nódulo.
       -- The Razer Seiren X microphone audio source node will be identified by matching exactly its node nickname.
-      { "node.nick", "equals", "Razer Seiren X" },
+      { 'node.nick', 'equals', 'Razer Seiren X' },
       -- Se identificará el nódulo de la entrada de audio del micrófono Razer Seiren X emparejando exactamente su clase de medios.
       -- The Razer Seiren X microphone audio source node will be identified by matching exactly its media class.
-      { "media.class", "equals", "Audio/Sink" },
+      { 'media.class', 'equals', 'Audio/Sink' },
     },
   },
   apply_properties = {
     -- Se deshabilitará el nódulo
     -- The node will be disabled.
-    ["node.disabled"] = true,
+    ['node.disabled'] = true,
   },
 }
 
-table.insert(alsa_monitor.rules,rule)
+table.insert(alsa_monitor.rules, rule)
--- .config/wireplumber/main.lua.d/56-nvidia-audio-sink-disable.lua
+++ .config/wireplumber/main.lua.d/56-nvidia-audio-sink-disable.lua
@@ -3,30 +3,30 @@ rule = {
     {
       -- Se identificará el nódulo de la salida de audio de la GPU Nvidia emparejando su nombre de nódulo.
       -- The 3.5mm jack audio sink node of the Nvidia GPU will be identified by matching its node name.
-      { "node.name",   "matches", "alsa_output*hdmi*" },
+      { 'node.name', 'matches', 'alsa_output*hdmi*' },
       -- Se identificará el nódulo de la salida de audio de la GPU Nvidia emparejando exactamente su apodo de nódulo.
       -- The 3.5mm jack audio sink node of the Nvidia GPU will be identified by matching exactly its node nickname.
-      { "node.nick",   "equals",  "VG27A" },
+      { 'node.nick', 'equals', 'VG27A' },
       -- Se identificará el nódulo de la salida de audio de la GPU Nvidia emparejando exactamente su clase de medios.
       -- The 3.5mm jack audio sink node of the Nvidia GPU will be identified by matching exactly its media class.
-      { "media.class", "equals",  "Audio/Sink" },
+      { 'media.class', 'equals', 'Audio/Sink' },
     },
     {
       -- Se identificará el nódulo de la salida de audio de la GPU Nvidia emparejando su nombre de nódulo.
       -- The 3.5mm jack audio sink node of the Nvidia GPU will be identified by matching its node name.
-      { "node.name",   "matches", "alsa_output*hdmi*" },
+      { 'node.name', 'matches', 'alsa_output*hdmi*' },
       -- Se identificará el nódulo de la salida de audio de la GPU Nvidia emparejando exactamente su apodo de nódulo.
       -- The 3.5mm jack audio sink node of the Nvidia GPU will be identified by matching exactly its node nickname.
-      { "node.nick",   "equals",  "ROG PG279QM" },
+      { 'node.nick', 'equals', 'ROG PG279QM' },
       -- Se identificará el nódulo de la salida de audio de la GPU Nvidia emparejando exactamente su clase de medios.
       -- The 3.5mm jack audio sink node of the Nvidia GPU will be identified by matching exactly its media class.
-      { "media.class", "equals",  "Audio/Sink" },
+      { 'media.class', 'equals', 'Audio/Sink' },
     },
   },
   apply_properties = {
     -- Se deshabilitará el nódulo
     -- The node will be disabled.
-    ["node.disabled"] = true,
+    ['node.disabled'] = true,
   },
 }
 
